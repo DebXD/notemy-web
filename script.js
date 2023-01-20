f2eac9aa251ada1012671d6c959a340da7e7429c
DebiprasadXD <debiprasaddaskdv@gmail.com>
2023-01-20 21:15:41 +0530

    add : login page

--- script.js
+++ script.js
@@ -1,157 +1,84 @@
+// JavaScript
+let token;
 
-let loginBtn = document.getElementById('loginbutton');
-
-
-let getToken = (loginCredentials) => {    
-    fetch("https://notemy-api.deta.dev/api/v1/auth/login/", {
-        method: 'POST',
-        headers: {'Content-Type' : 'application/json'},
-        body : JSON.stringify(loginCredentials)
+const fetchToken = (loginCredentials, getNotes) => {
+    return fetch("https://notemy-api.deta.dev/api/v1/auth/login/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginCredentials)
     })
-    .then(res => {if (res.status == 200){
-        console.log('Success')
-        //console.log(res)
-    
-    }else{
-        console.log('error')
-    }
-        return res.json()
-    })
-    .then(data => { return data['user']} )
-        
-    .then(tokens => {
-        //console.log(token);
-        let authorization_token = tokens['access token'];
-        let refreshToken = tokens['refresh token']
-        //document.getElementById('tokenfield').innerText=authorization_token;
-        return authorization_token, refreshToken;
-        })
-}
+    .then(response =>   response.json())
+    .then(data => {
+        token = data['user']['access token'];
+        console.log(token)
+        getMe(token);
+    });
+};
 
 
+function sleep(ms){
+        return new Promise( resolver => setTimeout(resolver, ms));
+    };
 
-
-const isTokenValid = (accessToken) => {
-    fetch("https://notemy-api.deta.dev/api/v1/auth/me/",{
-            method : 'GET',
-            headers : {
-                "Authorization" : 'Bearer ' + accessToken
-                        }
-                    })
-            .then(res => {
-                if (res.status != 200){
-                    return true
-                
-            }}
-            )} 
-const refreshAccessToken = (refreshToken) => {
-    fetch("https://notemy-api.deta.dev/api/v1/auth/token/refresh/",{
-                        method : 'POSt',
-                        headers : {
-                        "Authorization" : 'Bearer ' + refreshToken
-                        }
-                    })
-    .then(res => {
-        if (res.status == 200){
-            console.log('Access Token Regenerated')
-
-        }
-        else{
-            console.log('Error!')
-        }
-        return res.json()
-        
-    })
-    .then( data => {
-        return data['access token']
-    })
-    .then(token => {
-        authorization_token =  token;
-        return authorization_token
-    })
-
-}
-
-const getNotes = (accessToken, refreshToken, refreshAccessToken) => {
-    if (isTokenValid() == false){
-            console.log('token is not valid')
-            refreshAccessToken(refreshToken);
-            }
-    
-    fetch("https://notemy-api.deta.dev/api/v1/notes/",{
+const getMe = async (accessToken) => {
+    // let accessToken = fetchToken()
+    // console.log(accessToken);
+    //let accessToken = setTimeout(fetchToken();
+    await fetch("https://notemy-api.deta.dev/api/v1/auth/me/",{
         method : 'GET',
         headers : {
             "Authorization" : 'Bearer ' + accessToken
                     }
-                })
+        })
         .then(res => {
             if (res.status == 200){
             console.log('getting notes json data')
+            Swal.fire(
+                'Good job!',
+                'You have logged in.',
+                'success'
+              );
+                
             }
+            else{
+                console.log("Error getting userdata")
+            }
+            
             return res.json();
-        } )
-        .then( data => {return data.data})
+
+        })  
+        
+        .then( data => {
+            console.log(data);
+            return data.data;
+        })
         
         .then( items => {
-            for (item of items){
-                console.log(item['title'])
-                console.log(item['content'])
-                //document.getElementById('title').innerText=item['title'];
-                //document.getElementById('content').innerText=item['content']
-                
-            // notes = `
-            // <div id="title">${item['title']}</div>
-            // <div id="content">${item['content']}</div>`
-            // document.getElementById('container').innerHTML = notes 
-        } 
+            console.log(items)
+            
+            window.location.href = './notes.html'
+            
         })
         
 
-        }
+}
 
-loginBtn.addEventListener('click', () => {
-    console.log('button clicked')
+const loginBtn = document.getElementById('login-btn')
 
+loginBtn.addEventListener('click', () =>{
+    const inputEmail = document.getElementById('inputEmail').value;
+    const inputPassword = document.getElementById('inputPassword').value;
 
-    let user_email = document.getElementById("email-input").value;
-    console.log(user_email)
-    let user_password = document.getElementById('password-input').value;
-    console.log(user_password)
-    
-    
-    let loginCredentials = {
-        email : 'demo@email.com',
-        password : 'Demo@1234'
+    loginCredentials = {
+        email: inputEmail,
+        password: inputPassword
     };
-    
-    let accessToken, refreshToken = getToken(loginCredentials);
-    if (accessToken, refreshToken == undefined){
-        setTimeout(getNotes, 5000);
-        console.log(accessToken, refreshToken);
-        getNotes(accessToken, refreshToken, refreshAccessToken)
-        
-    }
-    ;   
-    })
-   // }
-    
+    console.log(loginCredentials)
 
-//})
+    fetchToken(loginCredentials, getMe)
 
-//     })
-            //  .then((data) => {
-            // for (item of data.data){
-            // //console.log(data['title'])
-            // //console.log(data['content'])
-            // //document.getElementById('title').innerText=item['title'];
-            // //document.getElementById('content').innerText=item['content']
-            
-            // notes = `
-            // <div id="title">${item['title']}</div>
-            // <div id="content">${item['content']}</div>`
-            // document.getElementById('container').innerHTML = notes
-//         }})
+})
 
-//     }
-// }
-// })
+// export {token};
