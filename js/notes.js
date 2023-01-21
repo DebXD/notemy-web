ba3b4f116777262fde93fa380fd6d10dc28bf91d
DebiprasadXD <debiprasaddaskdv@gmail.com>
2023-01-22 01:17:38 +0530

    show notes to authorized user

--- js/notes.js
+++ js/notes.js
@@ -1,6 +1,13 @@
 let token = localStorage.getItem("token");
-//console.log(token)
+
+const notesList = document.getElementById("note-container")
+
 if (!token){
+    Swal.fire(
+        'Logged out',
+        'Login again.',
+        'error'
+      );
     window.location.href = './login.html'
     console.log("token is null")
 }
@@ -9,7 +16,7 @@ else{
     // title.addEventListener('load', () => {
 
     const getNotes = (token) =>{
-        fetch('https://notemy-api.deta.dev/api/v1/notes/',{
+        fetch('https://notemy-api.deta.dev/api/v1/notes/?page=1',{
             method : "GET",
             headers : {
                 "Authorization" : 'Bearer ' + token
@@ -18,21 +25,29 @@ else{
         .then(res => {if (res.status ==200){
             return res.json();}
             else{
-                console.log("Uauthorized")
+                
                 window.location.href = './login.html'
             }
             }
             )
 
-        .then(data => { return data.data    })
+        .then(data => { return data.data  })
 
         .then(items => { let NotesLength = items.length;
-
+            let htmlString = ''
             for (let i=0; i< NotesLength; i++){
-                console.log(items[i]['id']);
-                console.log(items[i]['title']);
-                console.log(items[i]['content']);
+                htmlString += `
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${items[i]['title']}</h5>
+                            <p class="card-text">${items[i]['content']}</p> 
+                        </div>
+                    </div>
+                </div>
+              `
             }
+            notesList.innerHTML = htmlString;
         })
     }
     getNotes(token);
